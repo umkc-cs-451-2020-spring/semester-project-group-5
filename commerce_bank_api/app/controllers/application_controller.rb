46d107bc20064581ceca514a6e3872c32ce2c29e
mikelhomeless <mthomer1027@gmail.com>
2020-03-04 22:01:11 -0600

    modify 422 message

--- commerce_bank_api/app/controllers/application_controller.rb
+++ commerce_bank_api/app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
-  rescue_from ActiveRecord::RecordInvalid do |e|
-    record = e.record
-    render json: {errors: record.errors.full_messages}, status: :unprocessable_entity
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
+  def invalid_record(exception)
+    render json: {
+      message: 'Validation Failed', errors: exception.record.errors
+      }, status: :unprocessable_entity 
   end
 
   def not_found
-    render json: {error: "Not Found"}, status: :not_found
+    render json: {error: "Not Found"}, status: :not_found #404
   end
 end
