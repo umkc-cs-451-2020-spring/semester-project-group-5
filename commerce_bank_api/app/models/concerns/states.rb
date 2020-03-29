0c1389f95c029e658c05c899240042a007398972
mikelhomeless <mthomer1027@gmail.com>
2020-03-29 14:54:31 -0500

    added user validations

--- commerce_bank_api/app/models/concerns/states.rb
+++ commerce_bank_api/app/models/concerns/states.rb
@@ -1,3 +1,18 @@
 module States
   extend ActiveSupport::Concern
+
+  included do
+    validates :state,
+    presence: true,
+    inclusion: { in: us_states, message: "%{value} is not a valid state" }
+  end
+
+  def us_states
+    [
+      'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL',
+      'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE',
+      'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+      'VA', 'VT', 'WA', 'WI', 'WV', 'WY'
+    ]
+  end
 end
--- commerce_bank_api/app/models/user.rb
+++ commerce_bank_api/app/models/user.rb
@@ -3,6 +3,13 @@ class User < ApplicationRecord
   has_secure_password
 
   validates_presence_of :last_name, :first_name, :state
-  validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/}
+  validates :username,
+            presence: true,
+            uniqueness: true
+  validates :password,
+            :format: { with: /(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[`~!@#$%^&*()_+\-=+{}[\]\\|;':",.\/<>?]).{8,}/}
 end
