2f64ff62704cb0220bd1275eeb4b71afe2464805
mikelhomeless <mthomer1027@gmail.com>
2020-04-11 21:43:23 -0500

    fixed issues

--- commerce_bank_api/app/controllers/triggered_events_controller.rb
+++ commerce_bank_api/app/controllers/triggered_events_controller.rb
@@ -1,44 +1,27 @@
 class TriggeredEventsController < ApplicationController
-
-    before_action :find_triggered_event, only: [:show, :update, :destroy]
+    before_action :find_triggered_event, only: [:show]
 
     def create
-        @triggered_event = Triggered_event.create!(triggered_event_params)
-        render json: @user, status: :created
+        @triggered_event = TriggeredEvent.create!(triggered_event_params)
+        render json: @triggered_event, status: :created
       end
 
     def index
-        triggered_events=Triggered_event.order('created_at DESC')
-        render json: {status:'SUCCESS', message:'Loaded Triggered Events', data: triggered_events}, status: :ok
+        triggered_events = TriggeredEvent.all
+        render json: {status:'SUCCESS', message:'Loaded Triggered Events', events: triggered_events}, status: :ok
     end
 
     def show
-        triggered_events=Triggered_event.find(params[:id])
         render json: @triggered_event, status: :ok
-    end
-
-    def update
-        @triggered_event.update!(triggered_event_params)
-        head :no_content    
-    end
-    
-
-    def destroy
-        @triggered_event.destroy!
-        head : no_content
     end
 
     private
     def find_triggered_event
-        @triggered_event= Triggered_event.find(params[:id])
+        @triggered_event= TriggeredEvent.find(params[:id])
     end
 
     def triggered_event_params
-        params.permit(
-            :account_number,
-            :transaction_id,
-            :event_type,
-        )
+        params.permit(:event_type)
     end
 
 end
--- commerce_bank_api/app/models/triggered_event.rb
+++ commerce_bank_api/app/models/triggered_event.rb
@@ -1,4 +1,4 @@
 class TriggeredEvent < ApplicationRecord
-    validates_presence_of  :account_number :transaction_id :type
+    validates_presence_of :event_type
 end
 
