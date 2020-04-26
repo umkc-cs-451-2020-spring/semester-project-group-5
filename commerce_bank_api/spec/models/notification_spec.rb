33a8ccf37626b7fa89a4697b56410339d53dfd60
mikelhomeless <mthomer1027@gmail.com>
2020-04-25 21:48:32 -0500

    added unit tests for notifications

--- commerce_bank_api/spec/models/notification_spec.rb
+++ commerce_bank_api/spec/models/notification_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Notification, type: :model do
   # Validate that it belongs to a user
   # Validate that a message is generated
+
+  it { should belong_to(:user) }
+  it { should validate_presence_of(:message) }
 end
