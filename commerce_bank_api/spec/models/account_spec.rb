1132aac71ccab1f92953111462ba5c2bc5f50352
mikelhomeless <mthomer1027@gmail.com>
2020-04-25 21:29:08 -0500

    added user tests

--- commerce_bank_api/spec/models/account_spec.rb
+++ commerce_bank_api/spec/models/account_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validate that the account cannot drop below 0
+  # Validate that the account cannot go about 1,000,000
+  # Validate that an account number is generated prior to validation
 end
