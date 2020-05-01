055a3b914a104a2c52ded456e16c280feda5d937
Aanchal Tiwari <nidhitiwari0429@gmail.com>
2020-04-30 23:01:16 -0500

    Update

--- commerce-app/src/components/account.js
+++ commerce-app/src/components/account.js
@@ -7,50 +7,85 @@ return (
     <div className='Account'>
         <div className = 'header'>
             <h4>Account #****8989 -- Primary Savings</h4> 
-            <h6>Current Balance: $65687.00</h6>
+            <h6>Current Balance: $5500.00</h6>
         </div>
         <Table bordered striped>
 
-            <thead className='commerce-text'> 
+            <thead> 
                 <tr>
-                    <th className = 'commerce-text'>Transaction Number</th>
-                    <th className = 'commerce-text'>Description</th>
-                    <th className = 'commerce-text'>Ammount</th>
-                    <th className = 'commerce-text'>Category</th>
-                    <th className = 'commerce-text'>Date</th>
-                    <th className = 'commerce-text'>End Balance</th>
+                    <th>Transaction Number</th>
+                    <th>Description</th>
+                    <th>Amount</th>
+                    <th>Category</th>
+                    <th>Date</th>
+                    <th>End Balance</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
                     <td>1</td>
                     <td>Starbucks POS</td> 
-                    <td>$10</td>
+                    <td>-$10.00</td>
                     <td>Coffee</td> 
-                    <td>04/26/2020</td>
-                    <td>$990</td>
+                    <td>01/26/2020</td>
+                    <td>$5310.00</td>
                     <td><Button>Edit</Button>{' '}</td>
                 </tr>
                 <tr>
                     <td>2</td>
                     <td>Vietnamese Cafe</td> 
-                    <td>$20</td>
+                    <td>-$20.00</td>
                     <td>Food</td> 
-                    <td>04/26/2020</td>
-                    <td>$970</td>
+                    <td>02/02/2020</td>
+                    <td>$5290.00</td>
                     <td><Button>Edit</Button>{' '}</td>
                 </tr>
                 <tr>
                     <td>3</td>
                     <td>Beauty Salon</td> 
-                    <td>$50</td>
+                    <td>-$50.00</td>
                     <td>Products</td> 
-                    <td>04/26/2020</td>
-                    <td>$920</td>
+                    <td>03/08/2020</td>
+                    <td>$5240.00</td>
                     <td><Button>Edit</Button>{' '}</td>
                 </tr>
+
+
+                <tr>
+                    <td>4</td>
+                    <td>Cinema</td> 
+                    <td>-$20.00</td>
+                    <td>Entertainment</td> 
+                    <td>03/22/2020</td>
+                    <td>$5220.00</td>
+                    <td><Button>Edit</Button>{' '}</td>
+                </tr>
+
+                <tr>
+                    <td>5</td>
+                    <td>Salary</td> 
+                    <td>$500.00</td>
+                    <td>University of Missouri- Kansas City Payroll</td> 
+                    <td>04/10/2020</td>
+                    <td>$5720.00</td>
+                    <td><Button>Edit</Button>{' '}</td>
+                </tr>
+
+                <tr>
+                    <td>6</td>
+                    <td>ST. Lukes Hospital</td> 
+                    <td>-$200.00</td>
+                    <td>Health</td> 
+                    <td>04/26/2020</td>
+                    <td>$5520.00</td>
+                    <td><Button>Edit</Button>{' '}</td>
+                </tr>
+               
             </tbody>
+            
+            
         </Table>
+        <div> <Button variant="link" id="right-panel-link">Download Transactions in a CSV File</Button> </div>
     </div>
 
 );
