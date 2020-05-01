3ae1e51369bb04238361bdffe790ea50bda3c8f8
= <mthomer1027@gmail.com>
2020-04-30 21:19:05 -0500

    Added comments

--- commerce-app/src/utils/user-tracker.js
+++ commerce-app/src/utils/user-tracker.js
@@ -1,11 +1,13 @@
 const USER_SESSION_KEY = 'commerce-bank-app-user'
 const userTracker = () => {
-  const user = () => {
+  // This function will retrieve the user string from the session 
+  function user() {
     let userJSON = sessionStorage.getItem(USER_SESSION_KEY);
     return JSON.parse(userJSON);
   }
 
-  const setUser = (user) => {
+  // This function provides a way to set who the current user of the applicaiton is
+  function setUser(user) {
     let userJSON = JSON.stringify(user);
     sessionStorage.setItem(USER_SESSION_KEY, userJSON);
   }
